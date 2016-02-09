756495ef2f4d75c6fdbcf23b6ce7a3af7577fc60
Guy Oshiro <guyo714@icloud.com>
2016-02-09 12:53:16 -0800

    initial commit

---
+++ test/addnumbers.spec.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var chai = require('chai');
+//var addNumbers = require('../lib/add-numbers');
+
+chai.should();
+
+
+module.exports = function(){
+    describe('add numbers', function(){
+
+        it('should add correctly', function(){
+            true.should.be._false;
+            console.log('hello from test');
+        });
+
+        it('should add negative numbers');
+    });
+}
